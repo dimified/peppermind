a7ac1b327432031f2b5f88d5f3541a1781f54c10
Dimitri Reifschneider <dreifschneider.mmt-b2010@fh-salzburg.ac.at>
2013-01-09 14:45:41 +0100

    new rails version 3.2.8 initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Peppermind::Application.config.secret_token = '80b8d0c4408f1b4463eb69061206e3e98c11cf460c4b6f2ec9f6d811f72d5e1891315f872aa028f4f59559640c85f440cebea4b36a9ecc1574a3bd548601307f'
