7c4be5c9b68e5316bf05734e4704c879cef61df9
Dimitri Reifschneider <dreifschneider.mmt-b2010@fh-salzburg.ac.at>
2013-02-15 17:58:20 +0100

    user signup and login are implemented with flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+	def devise_mapping
+  	Devise.mappings[:user]
+	end
+
+	def resource_name
+  	devise_mapping.name
+	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -24,6 +24,12 @@ class User
   field :current_sign_in_ip, :type => String
   field :last_sign_in_ip,    :type => String
 
+  ## Custom Fields
+  field :display_name
+
+  ## Validations
+  validates :email, confirmation: true
+
   ## Confirmable
   # field :confirmation_token,   :type => String
   # field :confirmed_at,         :type => Time
