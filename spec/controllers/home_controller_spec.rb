3cfd4b346db27344c8f92f9c4b8a3748cf21d0ad
Sören Hentzschel <kontakt@soeren-hentzschel.at>
2013-02-16 09:42:28 +0100

    renamed home_controller spec

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -18,7 +18,7 @@ require 'spec_helper'
 # Message expectations are only used when there is no simpler way to specify
 # that an instance is receiving a specific message.
 
-describe HomesController do
+describe HomeController do
 
   # This should return the minimal set of attributes required to create a valid
   # Home. As you add validations to Home, be sure to
@@ -29,7 +29,7 @@ describe HomesController do
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
-  # HomesController. Be sure to keep this updated too.
+  # HomeController. Be sure to keep this updated too.
   def valid_session
     {}
   end
