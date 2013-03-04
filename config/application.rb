1f1b9eb4ec5ee331511e2e4db61dbf2522b50fba
Dimitri Reifschneider <dreifschneider.mmt-b2010@fh-salzburg.ac.at>
2013-03-04 10:38:58 +0100

    added additional settings for compass compilation

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,8 @@ module Peppermind
 
     # Forcing your application to not access the DB or load models when precompiling your assets.
     config.assets.initialize_on_precompile = false
+
+    # Precompile *all* assets, except those that start with underscore. You can also add files seperately.
+    #config.assets.precompile << /(^[^_\/]|\/[^_])[^\/]*$/
   end
 end
