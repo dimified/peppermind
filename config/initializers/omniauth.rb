4a06baf9ba76cacd37a21387c125d0c17ba96fdd
Dimitri Reifschneider <dreifschneider.mmt-b2010@fh-salzburg.ac.at>
2013-03-04 11:17:06 +0100

    modified omniauth.rb, loading environment variables

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,11 +1,9 @@
 require 'omniauth-openid'
 require 'openid/store/filesystem'
 
-API_KEYS = YAML::load_file(File.join(Rails.root, 'config', 'api_keys.yml'))[Rails.env]
-
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :browser_id, name: 'persona'
-  provider :facebook, API_KEYS['facebook']['key'], API_KEYS['facebook']['secret']
-  provider :twitter, API_KEYS['twitter']['key'], API_KEYS['twitter']['secret']
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
   provider :open_id, name: 'google', store: OpenID::Store::Filesystem.new('./tmp'), identifier: 'https://www.google.com/accounts/o8/id'
 end
