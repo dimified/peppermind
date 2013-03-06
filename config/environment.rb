ad4ba281cd533be65fdb08e7b388ac4c2f817831
Sören Hentzschel <kontakt@soeren-hentzschel.at>
2013-03-06 03:48:22 +0100

    load env vars from file if config file exists

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+# Load the environment variables from file if config file exists
+env_vars = File.join(Rails.root, 'config', 'env_vars.rb')
+load(env_vars) if File.exists?(env_vars)
+
 # Initialize the rails application
 Peppermind::Application.initialize!
