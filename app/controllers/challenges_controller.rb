1c88c9b657df9707b8d9eb29a14d111d91bc4b27
Sören Hentzschel <kontakt@soeren-hentzschel.at>
2013-05-17 14:28:28 +0200

    small optimization

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -14,6 +14,7 @@ class ChallengesController < ApplicationController
 
   def show
     @challenge = Challenge.find(params[:id])
+    @challenge.duration_string = convert_date @challenge.duration
 
     respond_to do |format|
       format.html
