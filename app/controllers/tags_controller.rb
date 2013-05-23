bbdcfe6a4cf43f73e01be387e8f89cb1766eceba
Sören Hentzschel <kontakt@soeren-hentzschel.at>
2013-05-23 20:07:49 +0200

    calculate tag size on server side instead of client side

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,14 @@
 class TagsController < ApplicationController
   def index
-    @tags = Challenge.tags_with_weight
+    tags = Challenge.tags_with_weight
+    @tags = []
+
+    tags.each do |tag|
+      size =  tag[1] / 10 + 1
+      size = 1 if size < 1
+      size = 2 if size > 2
+      @tags.push([tag[0], size.to_s])
+    end
   end
 
   def show
