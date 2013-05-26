942321aa0bcd90f52ec919a036a66fa79f1150cd
Sören Hentzschel <kontakt@soeren-hentzschel.at>
2013-05-26 03:12:01 +0200

    better tag cloud algorithm

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,12 +1,31 @@
 class TagsController < ApplicationController
   def index
+    # config and initialization
+    min_percent = 100
+    max_percent = 250
+    min = max = 1
+
+    # load tags
     tags = Challenge.tags_with_weight
+
+    # set minimum and maximum count for tag
+    tags.each do |tag|
+      count = tag[1].to_i
+      min = min > count ? count : min
+      max = count > max ? count : max
+    end
+
+    # prevent division by zero
+    max += 1 if min === max
+
+    # calculate offset
+    multiplier = (max_percent - min_percent) / (max - min)
+    offset = min_percent - min * multiplier
     @tags = []
 
+    # calculate font size and assign tags to view
     tags.each do |tag|
-      size =  tag[1] / 10 + 1
-      size = 1 if size < 1
-      size = 2 if size > 2
+      size = tag[1].to_i * multiplier + offset
       @tags.push([tag[0], size.to_s])
     end
   end
