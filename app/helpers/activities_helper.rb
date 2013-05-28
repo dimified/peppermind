559c94d3de7ba349609ba5a9c869738b49caa896
Dimitri Reifschneider <dimitri.reifschneider@gmail.com>
2013-05-28 19:50:05 +0200

    all tiles in activity stream list are now links

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -2,4 +2,13 @@ module ActivitiesHelper
 	def tracked_inspiration(trackable_id)
 		Inspiration.find(trackable_id)
 	end
+
+	def activity_link(type, trackable_id)
+    item = eval(type).find(trackable_id)
+		if type == "Challenge"
+			challenge_path(item)
+		elsif type == "Inspiration"
+		  challenge_inspiration_path(item.challenge, item)
+		end
+	end
 end
