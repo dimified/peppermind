5049d13b779a40120f890f532f4453bc6563e94a
Dimitri Reifschneider <dimitri.reifschneider@gmail.com>
2013-06-03 13:01:43 +0200

    updated font-awesome-rails gem (font-awesome-sass-rails was deprecated), spec tests updated, listing of last challenges in user show

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -26,7 +26,7 @@
 
   # Validations
   validates :question, presence: true, length: { maximum: 100 }
-  validates :description, presence: true, length: { maximum: 255 }
+  validates :description, presence: true, length: { maximum: 500 }
 
   # Number of Elements per Page
   paginates_per 10
--- spec/models/challenge_spec.rb
+++ spec/models/challenge_spec.rb
@@ -7,11 +7,11 @@ describe Challenge do
 	end
 
 	it "is invalid when title is more than 100 characters" do
-		expect(build(:challenge, question: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean ma")).to_not be_valid
+		expect(build(:challenge, question: Faker::Lorem.characters(101))).to_not be_valid
 	end
 
-	it "is invalid when description is more than 255 characters" do
-		expect(build(:challenge, description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis,q")).to_not be_valid
+	it "is invalid when description is more than 500 characters" do
+		expect(build(:challenge, description: Faker::Lorem.characters(501))).to_not be_valid
 	end
 
 	it "converts duration into duration_string" do 
